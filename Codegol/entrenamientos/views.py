9f0a22be4861d2c89b50f353db7e815052349ae9
Alex <castanedajefferson05@gmail.com>
2026-03-18 21:18:16 -0500

    feat: finaliza sección de entrenamientos con estilos y funcionalidad

--- Codegol/entrenamientos/views.py
+++ Codegol/entrenamientos/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Entrenamiento
 
-
 def lista_entrenamientos(request):
-    entrenamientos = Entrenamiento.objects.all()
-    return render(request, 'entrenamientos/lista.html', {'entrenamientos': entrenamientos})
+    query = request.GET.get('q')
+
+    if query:
+        entrenamientos = Entrenamiento.objects.filter(
+            descripcion__icontains=query,
+            estado=True
+        )
+    else:
+        entrenamientos = Entrenamiento.objects.filter(estado=True)
+
+    return render(request, 'entrenamientos/lista.html', {
+        'entrenamientos': entrenamientos,
+        'query': query
+    })
+
 
 
 def crear_entrenamiento(request):
     if request.method == 'POST':
         descripcion = request.POST.get('descripcion')
-        estado = request.POST.get('estado')
         lugar = request.POST.get('lugar')
         observaciones = request.POST.get('observaciones')
 
         Entrenamiento.objects.create(
             descripcion=descripcion,
-            estado=estado,
+            estado=True,  # 👈 SIEMPRE ACTIVO AL CREAR
             lugar=lugar,
             observaciones=observaciones
         )
@@ -31,14 +42,15 @@ def editar_entrenamiento(request, id):
 
     if request.method == 'POST':
         entrenamiento.descripcion = request.POST.get('descripcion')
-        entrenamiento.estado = request.POST.get('estado')
         entrenamiento.lugar = request.POST.get('lugar')
         entrenamiento.observaciones = request.POST.get('observaciones')
         entrenamiento.save()
 
         return redirect('lista_entrenamientos')
 
-    return render(request, 'entrenamientos/editar.html', {'entrenamiento': entrenamiento})
+    return render(request, 'entrenamientos/editar.html', {
+        'entrenamiento': entrenamiento
+    })
 
 
 def eliminar_entrenamiento(request, id):
