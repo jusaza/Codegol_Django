0dbfbe3532b5f8c754ce9726fca7ed2a72c3a593
Alex <castanedajefferson05@gmail.com>
2026-03-19 07:34:40 -0500

    Finalizacion Inventario

--- Codegol/codegol/settings.py
+++ Codegol/codegol/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuario',
     'entrenamientos',
+    'inventario',
     'codegol',
 ]
 
---
+++ Codegol/inventario/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_inventario, name='lista_inventario'),
+    path('crear/', views.crear_inventario, name='crear_inventario'),
+    path('editar/<int:id>/', views.editar_inventario, name='editar_inventario'),
+    path('eliminar/<int:id>/', views.eliminar_inventario, name='eliminar_inventario'),
+]
