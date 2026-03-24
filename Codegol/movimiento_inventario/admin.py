4de6c0b08df338dc9beeede37968da1a9e9bed43
Alex <castanedajefferson05@gmail.com>
2026-03-23 23:15:59 -0500

    sesion_entrenamiento y movimiento_inventario terimnao
    Falta arreglar algunos estilos

--- Codegol/movimiento_inventario/admin.py
+++ Codegol/movimiento_inventario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MovimientoInventario
 
-# Register your models here.
+@admin.register(MovimientoInventario)
+class MovimientoInventarioAdmin(admin.ModelAdmin):
+    list_display = ('id_movimiento', 'inventario', 'cantidad', 'tipo_movimiento', 'fecha')
+    search_fields = ('inventario__nombre_articulo',)
+    list_filter = ('tipo_movimiento', 'fecha')
+
