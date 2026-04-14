22b418dd4b40a88d0ed181f3580bae0dc5bba03c
jusaza_oficial <almanzasasajuliandavid4@gmail.com>
2026-04-14 07:08:56 -0500

    cache

--- Codegol/codegol/urls.py
+++ Codegol/codegol/urls.py
@@ -41,10 +41,4 @@ urlpatterns = [
     path('categoria/', include('categoria.urls')),
 
     path('rendimiento/', include('rendimiento.urls')),
-<<<<<<< HEAD
-
-    
-
-=======
->>>>>>> bf0ac046f03bd616608ad2ebce542800dfdc9174
 ]
